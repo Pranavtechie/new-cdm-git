9ac828647c083a66e874c740f87fee74f36f2b5c
pranav <pranav.techiegeek@gmail.com>
2023-01-28 03:45:05 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.svg" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload!
+        </p>
+        <span className="App-link">Hello from codedamn :)</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
